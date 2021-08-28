763f3921d8041609c2cf8094c03bc7dab00bc6c9
Nguyen Thanh Dung <dungynot@gmail.com>
2021-08-28 21:26:24 +0700

    cors config

--- src/main/java/com/example/demo/configuration/WebConfiguration.java
+++ src/main/java/com/example/demo/configuration/WebConfiguration.java
@@ -12,10 +12,7 @@ public class WebConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*")
-                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                registry.addMapping("/**");
             }
         };
     }

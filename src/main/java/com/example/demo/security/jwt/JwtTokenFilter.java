5d740335a26f75f87c10bf2429db2f5ff3bceed3
Nguyen Thanh Dung <dungynot@gmail.com>
2021-08-31 17:09:39 +0700

    update header for response

--- src/main/java/com/example/demo/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/example/demo/security/jwt/JwtTokenFilter.java
@@ -25,11 +25,29 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
 
+//        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+//        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+//        httpServletResponse.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+//        httpServletResponse.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+
+        // Access-Control-Allow-Origin
+        String origin = httpServletRequest.getHeader("Origin");
         httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpServletResponse.setHeader("Vary", "Origin");
+
+        // Access-Control-Max-Age
         httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
-        httpServletResponse.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
-        httpServletResponse.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+
+        // Access-Control-Allow-Credentials
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+
+        // Access-Control-Allow-Methods
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+
+        // Access-Control-Allow-Headers
+        httpServletResponse.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, " + "X-CSRF-TOKEN");
 
         try {
             String token = jwtTokenService.getJwtToken(httpServletRequest);
